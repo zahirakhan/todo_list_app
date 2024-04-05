75acfdfb312bc20863bf3fb91439107528b2d7a5
Zahira Khan <zahrakhan3567@gmail.com>
2024-04-05 03:03:35 -0700

    Added More Functions in My todo List App Like (Add Task, Update Task, View List, Delete Task , Exit)

--- index.ts
+++ index.ts
@@ -2,46 +2,75 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-let todo = [];
-let condition = true;
-
-while(condition){
-                    let addTask = await inquirer.prompt(
-                    [
-                        {
-                            name:"todo",
-                            type:"input",
-                            message:chalk.blueBright("What Do You Want To Add In Your ToDo List?"),
-                        },
-                        {
-                            name:`addMore`,
-                            type:"confirm",
-                            message:chalk.greenBright("Do You Want To Add More?"),
-                            default:"false",
-                        }
-                    ]);
-                    todo.push(addTask.todo); 
-                    condition =addTask.addMore
-                    console.log(todo)
-                }
-let clearList = await inquirer.prompt(
-    [
-        {
-            name: "clear",
-            type: "confirm",
-            message: chalk.redBright("Do you want to clear your ToDo list?"),
-            default: false,
-        },
-    ]);
-
-    if (clearList.clear) 
-    {
-        todo = [];
-        console.log(chalk.yellow("ToDo list cleared!"));
-    } else 
-    {
-        console.log(chalk.yellow("ToDo list not cleared!"));
-    }
-
-    console.log(chalk.yellow("Final ToDo List:"));
-    console.log(todo);
+let todoArray : string[] = [];
+async function makeTodo(todoArray:string[])
+{
+    do{
+        let outputList = await inquirer.prompt(
+            {
+                type:"list",
+                name:"choose",
+                message:"Select an Option.",
+                choices:["Add Task", "Update Task", "View List", "Delete Task", "Exit"]
+            })
+        if (outputList.choose === "Add Task")
+            {
+                let addTodo = await inquirer.prompt(
+                {
+                    type:"input",
+                    name:"addtodo",
+                    message:"Add Task in the List."
+                });
+                todoArray.push(addTodo.addtodo);
+                console.log(chalk.blueBright.italic.bold("Task Added Successfully!"));
+            
+            }
+        if (outputList.choose === "Update Task")
+            {
+                let updateTodo = await inquirer.prompt(
+                {
+                    name:"updatetodo",
+                    type:"list",
+                    message:"Enter Task for Update.",
+                    choices:todoArray.map(item => item)
+                });
+                let addTodo = await inquirer.prompt(
+                {
+                    type:"input",
+                    name:"addtodo",
+                    message:"Add Task in the List."
+                });
+                let newTodo = todoArray.filter(val => val !== updateTodo.updatetodo);
+                todoArray =[newTodo,addTodo.addtodo];
+                console.log(chalk.yellow.italic.bold("Task Updated Successfully!"));
+            
+            }
+        if (outputList.choose === "View List")
+            {
+                console.log(chalk.magentaBright.bold("\t","YOUR TODO LIST"));
+                todoArray.forEach(todo => console.log(chalk.green.italic.bold("\t\t",todo))); 
+                
+            }
+        if (outputList.choose === "Delete Task")
+            {
+                let deleteTodo = await inquirer.prompt(
+                    {
+                        name:"deletetodo",
+                        type:"list",
+                        message:"Enter Task for Delete.",
+                        choices:todoArray.map(item => item)
+                    });
+                    let newTodo = todoArray.filter(val => val !== deleteTodo.deletetodo);
+                    todoArray =[...newTodo];
+                    console.log(chalk.red.bold.italic(`"${deleteTodo.deletetodo}" is Successfully Deleted`));
+            
+            }
+        if (outputList.choose === "Exit")
+            {
+                console.log(chalk.greenBright.bold.italic("Todo List Exit Successfully!"));
+                break;
+            }
+   
+    } while(true); 
+}
+makeTodo(todoArray);
